387e7d68300eb18b0fedb2af6a759c34bfb33cf2
Jeff <JeffKo427@gmail.com>
2017-03-02 17:36:58 -0600

    Implemented Hvass Labs tutorial 2

---
+++ MNIST-3.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import time
+from datetime import timedelta
+import math
+
+import prettytensor as pt
