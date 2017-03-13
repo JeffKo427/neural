0be144194a089b5ecb042cc5cf318eb8ca7862f1
Jeff Alan <JeffKo427@gmail.com>
2017-03-13 15:53:14 -0500

    updated sort-and-batch.py

--- sort-and-batch.py
+++ sort-and-batch.py
@@ -4,4 +4,21 @@ import pickle
 import cv2
 import random
 
-data_dir = 'data/
+# Make a list of all the files we'll be working with.
+files1 = os.listdir('data/images_true')
+files0 = os.listdir('data/images_false')
+files_all = []
+
+for f in files1:
+    files_all.append(f)
+for f in files0:
+    files_all.append(f)
+
+# Randomize it.
+files_all.shuffle()
+
+# Randomly sort it into training and testing data.
+test = []
+train = []
+for f in files_all:
+
