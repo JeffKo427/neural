380b66499a802694012455653509e516562d2b24
Jeff <JeffKo427@gmail.com>
2017-03-21 21:05:01 -0500

    did stuff

--- maskmaker.py
+++ maskmaker.py
@@ -7,33 +7,43 @@ path = sys.argv[1]
 raw_imgs = os.listdir(sys.argv[1])
 random.shuffle(raw_imgs)
 
-block_size = 11
-C = 5
-for img in raw_imgs:
-    cv2.imread(path + img)
+block_size = 37
+C = 10
+for i in raw_imgs:
+    img = cv2.imread(path + i)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     hue, saturation, value = cv2.split(hsv)
 
     while True:
-        mask = cv2.adaptiveThreshold(
+        athresh = cv2.adaptiveThreshold(
                 src=saturation,
                 maxValue=255,
-                adaptive_method=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                threshold_type=cv2.THRESH_BINARY_INV,
-                block_size=block_size,
+                adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                thresholdType=cv2.THRESH_BINARY_INV,
+                blockSize=block_size,
                 C=C)
+        contours, hierarchy = cv2.findContours(athresh, cv2.RETR_EXTERNAL,     cv2.CHAIN_APPROX_NONE)
+        bigContours = []
+        for c in contours:
+             if cv2.contourArea(c) > 200:
+                 bigContours.append(c)
+
+        con = athresh.copy() * 0
+        cv2.drawContours(con, bigContours, -1, 255, -1)
+
         cv2.imshow('Original', img)
-        cv2.imshow('Mask', mask)
+        cv2.imshow('Mask', con)
 
         press = cv2.waitKey(0)
-        if press == 65362: #up arrow
+        print press
+        if 0xFF & press == 65362: #up arrow
             C -= 1
-        elif press == 65364: #down arrow
+        elif 0xFF & press == 65364: #down arrow
             C += 1
-        elif press == 119: #'w'
+        elif 0xFF & press == 119: #'w'
             #write
             break
-        elif press == 27: #Esc
+        elif 0xFF & press == 27: #Esc
             sys.exit()
         else:
             break
