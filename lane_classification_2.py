3796c44a5823072c2efb85a772383cad70f9b0c6
Jeff <JeffKo427@gmail.com>
2017-03-23 11:48:48 -0500

    modified stuff

--- lane_classification_2.py
+++ lane_classification_2.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 
 from keras.models import Model, load_model
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D, Reshape, BatchNormalization, ZeroPadding2D
+from keras.layers import Dense, Dropout, Activation, Flatten, Input
+from keras.layers import Conv2D, MaxPooling2D, Reshape, BatchNormalization, ZeroPadding2D
 from keras.utils import np_utils, plot_model
 from keras import backend as K
 from keras.callbacks import TensorBoard, ModelCheckpoint
@@ -25,24 +25,20 @@ input_shape = (image_size[0], image_size[1], 3)
 def buildModel():
     main_input = main_input = Input(shape=input_shape, name='main_input')
 
-    conv1_1 = Convolution2D(32, 3, 3, border_mode='same', name='conv1-1', activation='relu')
-    conv1 = Convolution2D(32, 3, 3, border_mode='same', name='conv1-2', activation='relu')(conv1_1)
+    conv1_1 = Conv2D(32, 3, 3, border_mode='same', name='conv1-1', activation='relu')(main_input)
+    conv1 = Conv2D(32, 3, 3, border_mode='same', name='conv1-2', activation='relu')(conv1_1)
     pool1 = MaxPooling2D((2,2), strides=(2,2), name='pool1')(conv1)
 
-    conv2_1 = Convolution2D(64, 3, 3, border_mode='same', name='conv2-1', activation='relu')(pool1)
-    conv2 = Convolution2D(64, 3, 3, border_mode='same', name='conv2-2', activation='relu')(conv2_1)
-    pool2 = MaxPooling2D((2,2), strides=(2,2), name='pool2')(conv1)
+    conv2_1 = Conv2D(64, 3, 3, border_mode='same', name='conv2-1', activation='relu')(pool1)
+    conv2 = Conv2D(64, 3, 3, border_mode='same', name='conv2-2', activation='relu')(conv2_1)
+    pool2 = MaxPooling2D((2,2), strides=(2,2), name='pool2')(conv2)
 
-    conv7 = Convolution2D(256, 90, 160, activation='relu', name='conv7')(Dropout(0.25)(pool2))
-    predict = Convolution2d(2,1,1)(Dropout(0.5))
+    fc6 = Flatten()(Dense(256, activation='relu', name='fc7')(Dropout(0.25)(pool2)))
+    fc7 = Dense(256, activation='relu')(fc6)
 
-    classification = Activation('softmax')(Flatten()(predict(conv7)))
-    FCN_4s = Upsampling2D(4)(predict(conv7))
-    FCN_2s = Upsampling2D(2)(predict(pool1))
-    semantic_labels = Add([FCN_2s, FCN_4s])
+    classification = Dense(2, activation='softmax')(fc6)
 
-    model = Model(inputs=[main_input], outputs=[classification]) #, FCN_4s, FCN_2s, semantic_labels])
-    model.load_weights(load_model('wtf.h5').get_weights(), by_name=True)
+    model = Model(main_input, classification) #, FCN_4s, FCN_2s, semantic_labels])
 
     model.compile(optimizer='adadelta', loss='categorical_crossentropy')  #, loss_weights=[1, 0, 0, 0])
 
@@ -66,18 +62,16 @@ validation_generator = datagen.flow_from_directory(
         batch_size=batch_size)
 
 
-modelName = 'VGG_lane_classifier.h5'
+modelName = 'functional_lane_classifier.h5'
 
+model = buildModel()
+'''
 vgg = VGG16(include_top=False, weights='imagenet', input_shape=input_shape)
-#for layer in vgg.layers:
- #   layer.trainable=False
-flat = Flatten()(vgg.output)
-fc1 = Dense(1024, activation='relu')(flat)
-#fc2 = Dense(1024, activation='relu')(fc1)
-pred = Dense(2, activation='softmax')(fc1)
-
-model = Model(vgg.input, pred)
-plot_model(model, 'vgg.png', show_shapes=True)
+weights = vgg.get_weights()
+for i in range(6):
+    model.layers[i].set_weights(weights[i])
+'''
+plot_model(model, 'flc.png', show_shapes=True)
 model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 
 checkpoint = ModelCheckpoint(
